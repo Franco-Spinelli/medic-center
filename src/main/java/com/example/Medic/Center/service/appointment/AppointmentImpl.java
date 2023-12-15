1e1c395e0466be9d941595d476bb12ceb0ae52d3
Franco-Spinelli <spinellifranco04@gmail.com>
2023-12-15 20:35:44 -0300

    changes in AppointmentImpl (save)

--- src/main/java/com/example/Medic/Center/service/appointment/AppointmentImpl.java
+++ src/main/java/com/example/Medic/Center/service/appointment/AppointmentImpl.java
@@ -1,7 +1,11 @@
 package com.example.Medic.Center.service.appointment;
 
+import com.example.Medic.Center.exception.RoomOccupiedException;
 import com.example.Medic.Center.model.entity.Appointment;
+import com.example.Medic.Center.model.entity.Bedroom;
+import com.example.Medic.Center.model.entity.Patient;
 import com.example.Medic.Center.repository.AppointmentRepository;
+import com.example.Medic.Center.service.bedroom.BedroomService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,6 +19,8 @@ import java.util.Optional;
 public class AppointmentImpl implements AppointmentService{
     @Autowired
     private AppointmentRepository appointmentRepository;
+    @Autowired
+    private BedroomService bedroomService;
 
     @Override
     public List<Appointment> findAll() {
@@ -29,6 +35,15 @@ public class AppointmentImpl implements AppointmentService{
 
     @Override
     public void save(Appointment appointment) {
+        List<Bedroom> bedrooms = appointment.getBedrooms();
+        Patient patient = appointment.getPatient();
+        for (Bedroom bedroom: bedrooms) {
+            try {
+                bedroomService.addPatientInBedroomById(patient.getId_patient(),bedroom.getId_bedroom());
+            }catch (RoomOccupiedException e){
+                System.out.printf(e.getMessage());
+            }
+        }
         appointmentRepository.save(appointment);
     }
 
